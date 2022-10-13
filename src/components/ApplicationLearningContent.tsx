8a5db1db37d8ecdd51668a96cb6d8958db255590
croquelois <croquelois.adrien@gmail.com>
2022-10-13 18:59:17 +0200

    :sparkles: add cursor on application learning scene

--- src/components/ApplicationLearningContent.tsx
+++ src/components/ApplicationLearningContent.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {useCurrentFrame, useVideoConfig} from 'remotion';
+import {Img, useCurrentFrame, useVideoConfig, staticFile} from 'remotion';
 import {Word} from '../helpers/Word';
 import {DiamondButton} from './button/DiamondButton';
 
@@ -12,10 +12,24 @@ export const ApplicationLearningContent: React.FC = () => {
 	const currentFrame = useCurrentFrame();
 	const [isReverse, setIsReverse] = useState(false);
 	const flipCardRef = useRef<HTMLDivElement>(null);
+	const cursorRef = useRef<HTMLImageElement>(null);
 
 	useEffect(() => {
-		if (currentFrame === 1.5 * fps) {
-			setIsReverse(true);
+		if (cursorRef.current !== null) {
+			if (currentFrame === 1) {
+				cursorRef.current.style.left = '1215px';
+				cursorRef.current.style.top = '80px';
+			}
+			if (currentFrame === 0.5 * fps) {
+				cursorRef.current.style.left = '635px';
+				cursorRef.current.style.top = '540px';
+			}
+			if (currentFrame === 1.5 * fps) {
+				setIsReverse(true);
+			}
+			if (currentFrame === 2 * fps) {
+				cursorRef.current.style.left = '730px';
+			}
 		}
 	}, [currentFrame]);
 
@@ -31,25 +45,32 @@ export const ApplicationLearningContent: React.FC = () => {
 	);
 
 	return (
-		<div className="flex flex-col justify-center mt-6 items-center gap-8">
-			<span className="text-2xl">{title}</span>
-			<div className="flip-card">
-				<div
-					ref={flipCardRef}
-					className={`flip-card-inner font-sans text-black text-2xl ${
-						isReverse ? 'reverse-card' : ''
-					}`}
-				>
-					<div className="flip-card-front flex flex-row mb-7 justify-center items-center">
-						<span>{word.foreignWord}</span>
-					</div>
-					<div />
-					<div className="flip-card-back flex flex-row mb-7 justify-center items-center">
-						<span>{word.nativeWord}</span>
+		<>
+			<div className="flex flex-col justify-center mt-6 items-center gap-8">
+				<span className="text-2xl">{title}</span>
+				<div className="flip-card">
+					<div
+						ref={flipCardRef}
+						className={`flip-card-inner font-sans text-black text-2xl ${
+							isReverse ? 'reverse-card' : ''
+						}`}
+					>
+						<div className="flip-card-front flex flex-row mb-7 justify-center items-center">
+							<span>{word.foreignWord}</span>
+						</div>
+						<div />
+						<div className="flip-card-back flex flex-row mb-7 justify-center items-center">
+							<span>{word.nativeWord}</span>
+						</div>
 					</div>
 				</div>
+				{bottomButtons}
 			</div>
-			{bottomButtons}
-		</div>
+			<Img
+				ref={cursorRef}
+				className="cursor-transition w-5 h-auto absolute z-40"
+				src={staticFile('icons/cursor.svg')}
+			/>
+		</>
 	);
 };
--- src/scenes/TextScene.tsx
+++ src/scenes/TextScene.tsx
@@ -3,12 +3,15 @@ import {AbsoluteFill} from 'remotion';
 
 export const TextScene: React.FC<{message: string}> = (props) => {
 	return (
-		<AbsoluteFill className="bg-darkGrey">
+		<AbsoluteFill className="bg-darkGrey z-50">
 			<div className="w-full h-full flex flex-col relative">
-				<div className="flex-auto w-full"/>
-				<div className="w-full h-12 bg-primary bottom-text-radius"/>
+				<div className="flex-auto w-full" />
+				<div className="w-full h-12 bg-primary bottom-text-radius" />
 				<div className="absolute flex flex-row justify-center items-center w-full h-full px-20">
-					<span className="text-7xl leading-tight text-center" dangerouslySetInnerHTML={{__html: props.message}} />
+					<span
+						className="text-7xl leading-tight text-center"
+						dangerouslySetInnerHTML={{__html: props.message}}
+					/>
 				</div>
 			</div>
 		</AbsoluteFill>
